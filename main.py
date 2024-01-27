ca2e3cd0173c312cf89a9bb608f6126cda242082
Yusuf <121329421+yssfklc@users.noreply.github.com>
2024-01-27 17:59:31 +0300

    fix pinecone init error
    fix pinecone init error

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import os
-from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.llms.openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
 import pinecone
 
-pinecone.init(
+print(os.getenv("PINECONE_API_KEY"))
+
+
+pinecone.Pinecone(
     api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
     environment=os.getenv("PINECONE_ENV"),  # next to api key in console
 )
 
 if __name__ == '__main__':
     print('Hello Yusuf, Welcome to the new project')
-    loader = TextLoader("../mediumblogs/ediumblog1.txt")
+    loader = TextLoader(file_path='./mediumblog1.txt', encoding= None, autodetect_encoding= True)
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
+    )
     docs = text_splitter.split_documents(documents)
 
     embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get('OPENAI_API_KEY'))
